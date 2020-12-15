95e4a4ae212bb744a694e94570e2c2343ffe9427
jkchen <835858556@qq.com>
2020-12-15 14:57:07 +0800

    多图上传

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -4,31 +4,32 @@ const os = require('os')
 
 const essayWrite = async (ctx,next) =>{
     if(ctx.method != "POST") return await next();
-    
-    // console.log(ctx.request.files.file)
 
-    if(ctx.request.files.length > 0){
-        let imgarr = []
-        for(let file of ctx.request.files){
-            const reader = fs.createReadStream(file.path)
-            let newName = Date.now() + file.name.split('.').shift() + '.' + file.name.split('.').pop();
-            const newPath = path.join(__dirname,'../public/images/'+newName)
-            const stream = fs.createWriteStream(newPath)
+    let files = ctx.request.files
+    let title = ctx.request.body.title
 
-            // console.log(stream.path)
-            reader.pipe(stream);
-            imgarr.push(newPath)
+    let result = await new Promise((resolve,reject)=>{
+        if(Object.keys(files).length > 0){
+            let imgarr = [];
+            Object.keys(files).forEach(key=>{
+                let file = files[key]
+                const reader = fs.createReadStream(file.path)
+                let newName = Date.now() + file.name.split('.').shift() + '.' + file.name.split('.').pop();
+                const newPath = path.join(__dirname,'../public/images/'+newName)
+                const stream = fs.createWriteStream(newPath)
+                reader.pipe(stream);
+                imgarr.push(newPath)
+            })
+            resolve(imgarr)
+        }else{
+            console.log("异步catch")
+            reject(Object.keys(files).length)
+
         }
-    }
-    ctx.body = '写入成功'
-    // const file = ctx.request.files.file;
-    // const reader = fs.createReadStream(file.path)
-    // let newName = Date.now() + file.name.split('.').shift() + '.' + file.name.split('.').pop();
-    // const newPath = path.join(__dirname,'../public/images/'+newName)
-    // const stream = fs.createWriteStream(newPath)
-    // console.log(stream.path)
-    // reader.pipe(stream);
-    // ctx.body = newPath
+    }).then(res=>{
+        let data = [title,res];
+        ctx.body = data
+    })
 }
 const essayedit = async ctx =>{
     ctx.body = "编辑成功"
